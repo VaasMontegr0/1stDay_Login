e9f60c17fab0419d034dd58cf9cca625cb3e9ac1
VaasMontegr0 <cyclonesanjay.cs@gmail.com>
2019-12-09 13:35:06 +0530

    Check

--- src/test/java/Prime.java
+++ src/test/java/Prime.java
@@ -7,7 +7,7 @@ public static void main(String[] args)
 
 Scanner s=new Scanner(System.in);	
 	System.out.println("Enter the number for factorial");
-	
+System.out.println("New check for Git");	
 	int n=s.nextInt();
 	int factorial=1;
 	for(int i=1; i<=n; i++) {
